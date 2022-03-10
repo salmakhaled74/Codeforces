77c81a5b5ebfa01b3e17ecd65e7538fe062b16bd
Salma <77203008+salmakhaled74@users.noreply.github.com>
2022-03-10 21:54:14 +0200

    Create (A) Nearly Lucky Number.cpp

---
+++ (A) Nearly Lucky Number.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    int cnt=0;
+    for(int i=0; i<s.size(); i++){
+        if(s[i] == '4' || s[i] == '7'){
+            cnt++;
+        }
+    }
+    if(cnt == 7 || cnt == 4){
+        cout << "YES";
+    }else{
+        cout << "NO";
+    }
+    return 0;
+}
